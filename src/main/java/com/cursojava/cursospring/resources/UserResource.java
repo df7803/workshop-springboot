0d1b992503faac91f701a36b9d59da10567d3a59
unknown <df7803@gmail.com>
2024-02-21 20:22:07 -0300

    adicionando usuario

---
+++ src/main/java/com/cursojava/cursospring/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.cursojava.cursospring.resources;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursojava.cursospring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@gmail.com", "999999999", "12345" );
+		return ResponseEntity.ok().body(u);
+	}
+}
